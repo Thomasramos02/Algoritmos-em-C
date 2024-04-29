dd233ec4095078f6518bfe9febc07bab68dcc9e8
Thomas Ramos Oliveira <161534687+Thomasramos02@users.noreply.github.com>
2024-04-29 11:17:12 -0300

    Add files via upload

---
+++ Algoritmos e Dados C/Array1/src/Array1.c
@@ -0,0 +1,36 @@
+/*
+ ============================================================================
+ Name        : Array1.c
+ Author      : Thomas ramos
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int numeros[5];
+	int soma = 0;
+
+
+	printf("Digite 5 numeros inteiros: \n");
+
+	for(int i = 0; i < 5; i++){
+		printf("Numero %d:",i + 1);
+		scanf("%d",&numeros[i]);
+		soma = soma + numeros[i];
+
+	}
+
+	double media = (double) soma/5;
+
+	printf("Soma: %d\n",soma);
+	printf("Media: %.2lf\n",media);
+
+
+
+	return 0;
+}
