cdece4f0d21de65f6a392ee003e89a0287960d5d
Thomas Ramos Oliveira <161534687+Thomasramos02@users.noreply.github.com>
2024-05-20 11:54:48 -0300

    Add files via upload

---
+++ Algoritmos e Dados C/SomarArray/src/cemig.c
@@ -0,0 +1,8 @@
+/*
+ ============================================================================
+ Name        : cemig.c
+ Author      : Thomas ramos
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ===
