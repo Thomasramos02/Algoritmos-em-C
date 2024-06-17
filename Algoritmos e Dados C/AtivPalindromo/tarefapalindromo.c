0ba8d7a99c5420b233c71111ef3aa152b771c63e
Thomas Ramos Oliveira <161534687+Thomasramos02@users.noreply.github.com>
2024-06-17 10:29:11 -0300

    Atualizar o tarefapalindromo.c

--- Algoritmos e Dados C/AtivPalindromo/tarefapalindromo.c
+++ Algoritmos e Dados C/AtivPalindromo/tarefapalindromo.c
@@ -1,42 +1,28 @@
-/*
- ============================================================================
- Name        : tarefapalindromo.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int main(void) {
-	char palavra[100];
-	int i, j, tamanho;
-	int palindromo = 1;
+int main() {
+    char resultado;
+    int vitorias = 0;
 
-	printf("Digite uma palavra: ");
-	fflush(stdout);
-	scanf("%s",palavra);
-	tamanho = strlen(palavra);
+    // Leitura dos resultados dos 6 jogos
+    for (int i = 0; i < 6; i++) {
+        scanf(" %c", &resultado);
+        if (resultado == 'V') {
+            vitorias++;
+        }
+    }
 
-	for(i = 0, j = tamanho - 1; 1 < j; i++, j--){
-		printf("i: %c\n", palavra[i]);
-		printf("j: %c\n", palavra[j]);
-		if(palavra[1] != palavra[j]){
-			palindromo = 0;
-			break;
-		}
-	}
-	if(palindromo){
-		printf("\nA palavra é um palindromo. \n");
-	}else{
-		printf("\nA palavra nao é um palindromo. \n");
-	}
+    // Determinação do grupo com base no número de vitórias
+    if (vitorias == 5 || vitorias == 6) {
+        printf("1\n");
+    } else if (vitorias == 3 || vitorias == 4) {
+        printf("2\n");
+    } else if (vitorias == 1 || vitorias == 2) {
+        printf("3\n");
+    } else {
+        printf("-1\n");
+    }
 
-
-
-	return EXIT_SUCCESS;
+    return 0;
 }
+
