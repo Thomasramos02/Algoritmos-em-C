240edc19c6b652fbefb963473d0b4976c47a7064
Thomas Ramos Oliveira <161534687+Thomasramos02@users.noreply.github.com>
2024-06-17 10:24:28 -0300

    Atualizar o AtivCigarros.c

--- Algoritmos e Dados C/AtivCigarros/AtivCigarros.c
+++ Algoritmos e Dados C/AtivCigarros/AtivCigarros.c
@@ -1,29 +1,34 @@
-#include<stdlib.h>
 #include <stdio.h>
 
-int cigarrosAoLongoVida(int cigarrosDia, int numeroAnos) {
-    return cigarrosDia * numeroAnos * 365;
-}
+int main() {
+    int N;
+    scanf("%d", &N);
 
-float totalGastoEmCigarros(int cigarrosAoLongoVida, float precoCigarro){
-    return (cigarrosAoLongoVida / 20.0) * precoCigarro;
-}
+    if (N < 0 || N > 10) {
+        printf("Número fora do intervalo permitido.\n");
+        return 1;
+    }
 
-int main(void){
-    int numeroAnos, cigarrosDia;
-    float precoCigarro;
+    int esquerda = (N <= 5) ? N : 5;
+    int direita = (N <= 5) ? 0 : N - 5;
 
-    printf("Quantidade de anos que fumas: ");
-    scanf("%d", &numeroAnos);
-    printf("Cigarros por dia: ");
-    scanf("%d", &cigarrosDia);
-    printf("Preço médio do cigarro em reais: ");
-    scanf("%f", &precoCigarro);
+    // Representação da mão esquerda
+    for (int i = 0; i < esquerda; i++) {
+        printf("I");
+    }
+    for (int i = esquerda; i < 5; i++) {
+        printf("*");
+    }
+    printf("\n");
 
-    int totalCigarros = cigarrosAoLongoVida(cigarrosDia, numeroAnos);
-    float totalGasto = totalGastoEmCigarros(totalCigarros, precoCigarro);
+    // Representação da mão direita
+    for (int i = 0; i < direita; i++) {
+        printf("I");
+    }
+    for (int i = direita; i < 5; i++) {
+        printf("*");
+    }
+    printf("\n");
 
-    printf("Total de Cigarros Fumados: %d\n", totalCigarros);
-    printf("Total gasto em cigarros ao longo da vida: R$ %.2f\n ", totalGasto);
     return 0;
 }
